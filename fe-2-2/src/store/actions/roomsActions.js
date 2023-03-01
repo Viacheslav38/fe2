627ad66987e7dc597bee667391766596d4f6e567
Viacheslav38 <124434961+Viacheslav38@users.noreply.github.com>
2023-03-01 10:48:51 +0200

    Delete fe-2-2 directory

--- fe-2-2/src/store/actions/roomsActions.js
+++
@@ -1,4 +0,0 @@
-import ACTION_TYPES from '../../constants/actionTypes';
-
-export const getRooms = () => ({ type: ACTION_TYPES.GET_ROOMS });
-export const getRoomsSuccess = (roomsData) => ({ type: ACTION_TYPES.GET_ROOMS_SUCCESS, payload: roomsData });
--- fe-2-2/src/store/selectors/roomsSelectors.js
+++
@@ -1,1 +0,0 @@
-export const getRoomsState = (state) => Object.values(state.rooms);
