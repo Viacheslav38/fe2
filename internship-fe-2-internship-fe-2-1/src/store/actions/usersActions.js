e6fd30c303ca03418773b070b3f9a0b87409464f
Viacheslav38 <124434961+Viacheslav38@users.noreply.github.com>
2023-03-01 10:48:15 +0200

    Delete internship-fe-2-internship-fe-2-1 directory

--- internship-fe-2-internship-fe-2-1/src/store/actions/usersActions.js
+++
@@ -1,4 +0,0 @@
-import ACTION_TYPES from '../../constants/actionTypes';
-
-export const getAccounts = () => ({ type: ACTION_TYPES.GET_ACCOUNTS });
-export const getAccountsSuccess = (accountData) => ({ type: ACTION_TYPES.GET_ACCOUNTS_SUCCESS, payload: accountData });
--- internship-fe-2-internship-fe-2-1/src/store/selectors/usersSelectors.js
+++
@@ -1,1 +0,0 @@
-export const getAccountsState = (state) => state.users.accounts;
